0b896a5757d13d00691d27d9a93496e3dd8ae8d1
HONGSEONGCHAN <seongchan_@naver.com>
2020-08-26 22:32:14 +0900

    Compare Hook and Class

--- React/2.js
+++ React/2.js
@@ -16,6 +16,13 @@ const Hook = () => { // 함수 컴포넌트라고 부름
     const [value, setValue] = React.useState(''); // state를 비어는 것으로 선언하겠다
     const [result, setResult] = React.useState('');
     const inputRef = React.useRef(null); // ref 쓰는 방법. (초기값)
+    
+    // const [state, setState] = React.useState({
+    //     first: Math.ceil(Math.random() * 9), // 첫 번째 숫자 랜덤(1~9)
+    //     second: Math.ceil(Math.random() * 9), // 두 번째 숫자 랜덤(1~9)
+    //     value: '',
+    //     result: '',
+    // }); 이렇게 사용도 가능하지만 setState를 할 때 문제가 발생할 수 있음. setState할 때 하나만 바꾸려고 해도 모두를 다 바꿔야함
 
     const onChangeInput = (e) => {
         setValue(e.target.value);
@@ -25,6 +32,9 @@ const Hook = () => { // 함수 컴포넌트라고 부름
         e.preventDefault();
         if(parseInt(value) === first * second) {
             setResult(first + ' X ' + second + ' = ' + value + ', 정답!');
+            // setResult((prevResult) => {
+            //     return prevResult + ', 정답!';
+            // }); => 함수형으로 사용 가능
             setFirst(Math.ceil(Math.random() * 9));
             setSecond(Math.ceil(Math.random() * 9));
             setValue('');
@@ -36,6 +46,7 @@ const Hook = () => { // 함수 컴포넌트라고 부름
         }
     }
 
+    // onSubmit을 했을 때 React는 setState 인식하고 한 번에 해결. 즉 onSubmit을 했을 때 렌더링은 한 번만!
     return <React.Fragment>
         <div>{first} X {second}의 값은?</div>
         <form onSubmit={onSubmitForm} className="forms">
