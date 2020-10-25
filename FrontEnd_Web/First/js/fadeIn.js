67bc81eccb96ac630fb4a061a7be27a50cc80422
HONGSEONGCHAN <seongchan_@naver.com>
2020-10-25 20:14:02 +0900

    Modify a WebSite

--- FrontEnd_Web/First/js/fadeIn.js
+++ FrontEnd_Web/First/js/fadeIn.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
-            if(bottom_of_window > bottom_of_object / 2){
+            if(bottom_of_window > bottom_of_object - 250){
                 $(this).animate({'opacity':'1'}, 500);
             }
         });
