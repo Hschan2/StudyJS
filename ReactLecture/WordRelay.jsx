a422cec32ac409bec0015a4115faa122866320d5
HONGSEONGCHAN <seongchan_@naver.com>
2020-11-03 20:57:21 +0900

    Write a WordRelay Code

--- ReactLecture/WordRelay.jsx
+++ ReactLecture/WordRelay.jsx
@@ -4,11 +4,54 @@ const {Component} = React;
 
 class WordRelay extends Component {
     state = {
-        text: "hello, webpack",
+        word: "강아지",
+        value: '',
+        result: '',
     };
 
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if(this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState({
+                result: "좋아요",
+                word: this.state.value,
+                value: '',
+            })
+            this.input.focus();
+        } else {
+            this.setState({
+                result: "다시!",
+                word: '',
+                value: '',
+            })
+            this.input.focus();
+        }
+    }
+
+    input;
+
+    onRefInput = (c) => {
+        this.input = c;
+    }
+
+    onChangeInput = (e) => {
+        this.setState({
+            value: e.target.value,
+
+        });
+    }
+
     render() {
-    return <div>{this.state.text}</div>;
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>입력</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
     }
 }
 
--- ReactLecture/client.jsx
+++ ReactLecture/client.jsx
@@ -4,4 +4,4 @@ const ReactDom = require('react-dom');
 const GuGuDan = require('./GuGuDan');
 const WordRelay = require('./WordRelay'); // WordRelay.jsx 불러오기. webpack이 알아서 파악하고 무엇이 무엇을 불러와야 하는지 스스로 동작
 
-ReactDom.render(<GuGuDan />, document.querySelector('#root')); // jsx 문법
+ReactDom.render(<WordRelay />, document.querySelector('#root')); // jsx 문법
