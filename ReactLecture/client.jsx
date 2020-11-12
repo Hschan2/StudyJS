c9ac3f5d6b52083718a4c1a0447f2827151831a3
HONGSEONGCHAN <seongchan_@naver.com>
2020-11-12 13:52:33 +0900

    Baseball Game

--- ReactLecture/client.jsx
+++ ReactLecture/client.jsx
@@ -4,7 +4,8 @@ const {hot} = require('react-hot-loader/root'); // 웹 수정을 실시간으로
 
 const GuGuDan = require('./GuGuDan');
 const WordRelay = require('./WordRelay'); // WordRelay.jsx 불러오기. webpack이 알아서 파악하고 무엇이 무엇을 불러와야 하는지 스스로 동작
+const baseBall = require(`./Baseball`);
 
-const Hot = hot(WordRelay); // 수정 사항 있을 시 알아서 반영
+const Hot = hot(baseBall); // 수정 사항 있을 시 알아서 반영
 
 ReactDom.render(<Hot />, document.querySelector('#root')); // jsx 문법
--- ReactLecture/try.js
+++ ReactLecture/try.js
@@ -5,7 +5,7 @@ class Try extends Component { // Component => 재사용성
         return (
             // 부모 .jsx에서 넘긴 이름 그대로 가져와야 한다. Baseball.jsx에서 value={v} index={i}로 선언하였으니 value와 index로 가져와야 한다.
             // <li key={this.props.value.fruit + this.props.value.taste}><b>{this.props.index} - {this.props.value.fruit}</b> - {this.props.value.taste}</li>
-            <li>
+            <li style={{listStyle:"none"}}>
                 {/* tryInfo 안에는 try와 result가 있기 때문에 */}
                 <div>{this.props.tryInfo.try}</div>
                 <div>{this.props.tryInfo.result}</div>
