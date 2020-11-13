5edafd1f30043b7930f4f02ed06fbbc4cc0ef725
HONGSEONGCHAN <seongchan_@naver.com>
2020-11-13 14:11:05 +0900

    Number Baseball Game Hook version

--- ReactLecture/Baseball.jsx
+++ ReactLecture/Baseball.jsx
@@ -1,89 +1,161 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import Try from './try.js';
 
-function getNumbers() {
+// 외부에서 작성한 함수는 Hook 영향 X
+function getNumbers() { // this를 사용하지 않을 때. class 안에다 사용해도 된다(이때는 this.getNumbers()로)
     const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const array = [];
     for(let i = 0; i < 4; i++) {
         const chosen = candidate.splice(Math.floor(Math.random * (9 - i)), 1)[0];
         array.push(chosen);
     }
+    return array;
 }
 
-class Baseball extends Component {
-    state = {
-        result: '',
-        value: '',
-        answer: getNumbers(), // 랜덤 숫자가 추출
-        tries: [], // React에서 배열에 값을 넣을 때, push 사용 비추천
-    };
+// Hook ver.
+const Baseball = () => {
+    const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
+    const [answer, setAnswer] = useState(getNumbers());
+    const [tries, setTries] = useState([]);
 
-    onSubmitForm = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if(this.state.value === this.state.answer.join('')) { // 맞추었을 때
-            this.setState({
-                result: '홈런',
-                tries: [...this.state.tries, {try: this.state.value, result: '홈런'}], // ... => 기존 배열 복사 (기존 배열 안에 값도 복사), {} => 넣을 값
+        if(value === answer.join('')) {
+            setResult('홈런');
+            setTries((prevTries) => { // 옛날 걸로 현재 걸 만든다 => 함수형
+                return [...prevTries, {try: value, result: '홈런'}];
             });
             alert('게임을 다시 시작합니다.');
-            this.setState({
-                value: '',
-                answer: getNumbers(),
-                tries: [],
-            });
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
         } else { // 틀렸을 때
-            if(this.state.tries.length >= 9) {
-                this.setState({
-                    result: `10번 이상 시도로 게임 종료, 답은 ${this.state.answer.join(',')}`,
-                });
+            if(tries.length >= 9) {
+                setResult(`10번 이상 시도로 게임 종료, 답은 ${answer.join(',')}`);
                 alert('게임을 다시 시작합니다.');
-                this.setState({
-                    value: '',
-                    answer: getNumbers(),
-                    tries: [],
-                });
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
             } else {
                 for(let i = 0; i < 4; i++) {
-                    if(answerArray[i] === this.state.answer[i]) {
+                    if(answerArray[i] === answer[i]) {
                         strike++;
-                    } else if(this.state.answer.includes(answerArray[i])) {
+                    } else if(answer.includes(answerArray[i])) {
                         ball++;
                     }
                 }
-                this.setState({
-                    tries: [...this.state.tries, {try: this.state.value, result: '${strike} 스트라이크, ${ball} 볼 입니다.'}],
-                    value: '',
+                setTries((prevTries) => {
+                    return [...prevTries, {try: value, result: '${strike} 스트라이크, ${ball} 볼 입니다.'}];
                 });
+                setValue('');
             }
         }
     };
 
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.target.value,
-        });
-    }
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    };
 
-    render() {
-        return (
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-                <button>입력</button>
-                </form>
-                <div>시도: {this.state.tries.length}</div>
-                <ul>
-                    {this.state.tries.map( (v, i) => { // v는 value, i는 index
-                        return (
-                            // index를 key쓰지 말자. (이 경우에는 학습이기 때문에 예외)
-                            <Try key={`${i+1}차 시도 : `} tryInfo={v}></Try>
-                        );
-                    })}
-                </ul>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+            <input maxLength={4} value={value} onChange={onChangeInput} />
+            <button>입력</button>
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v, i) => { // v는 value, i는 index
+                    return (
+                        // index를 key쓰지 말자. (이 경우에는 학습이기 때문에 예외)
+                        <Try key={`${i+1}차 시도 : `} tryInfo={v}></Try>
+                    );
+                })}
+            </ul>
+        </>
+    );
+};
+
+// class Baseball extends Component {
+//     state = {
+//         result: '',
+//         value: '',
+//         answer: getNumbers(), // 랜덤 숫자가 추출
+//         tries: [], // React에서 배열에 값을 넣을 때, push 사용 비추천
+//     };
+
+//     onSubmitForm = (e) => {
+//         const {value, tries, answer} = this.state;
+//         e.preventDefault();
+//         if(value === answer.join('')) { // 맞추었을 때
+//             this.setState((prevState) => { // 옛 state를 사용해 현재 state로 만들 때 함수형으로
+//                 return {
+//                     result: '홈런',
+//                     tries: [...prevState.tries, {try: value, result: '홈런'}], // ... => 기존 배열 복사 (기존 배열 안에 값도 복사), {} => 넣을 값
+//                 }
+//             });
+//             alert('게임을 다시 시작합니다.');
+//             this.setState({
+//                 value: '',
+//                 answer: getNumbers(),
+//                 tries: [],
+//             });
+//         } else { // 틀렸을 때
+//             if(tries.length >= 9) {
+//                 this.setState({
+//                     result: `10번 이상 시도로 게임 종료, 답은 ${answer.join(',')}`,
+//                 });
+//                 alert('게임을 다시 시작합니다.');
+//                 this.setState({
+//                     value: '',
+//                     answer: getNumbers(),
+//                     tries: [],
+//                 });
+//             } else {
+//                 for(let i = 0; i < 4; i++) {
+//                     if(answerArray[i] === answer[i]) {
+//                         strike++;
+//                     } else if(answer.includes(answerArray[i])) {
+//                         ball++;
+//                     }
+//                 }
+//                 this.setState((prevState) => {
+//                     return {
+//                         tries: [...prevState.tries, {try: value, result: '${strike} 스트라이크, ${ball} 볼 입니다.'}],
+//                         value: '',
+//                     }
+//                 });
+//             }
+//         }
+//     };
+
+//     onChangeInput = (e) => {
+//         this.setState({
+//             value: e.target.value,
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <h1>{this.state.result}</h1>
+//                 <form onSubmit={this.onSubmitForm}>
+//                 <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+//                 <button>입력</button>
+//                 </form>
+//                 <div>시도: {this.state.tries.length}</div>
+//                 <ul>
+//                     {this.state.tries.map( (v, i) => { // v는 value, i는 index
+//                         return (
+//                             // index를 key쓰지 말자. (이 경우에는 학습이기 때문에 예외)
+//                             <Try key={`${i+1}차 시도 : `} tryInfo={v}></Try>
+//                         );
+//                     })}
+//                 </ul>
+//             </>
+//         );
+//     }
+// }
 
 module.exports = Baseball;
--- ReactLecture/try.js
+++ ReactLecture/try.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
 
-class Try extends Component { // Component => 재사용성
-    render() {
-        return (
-            // 부모 .jsx에서 넘긴 이름 그대로 가져와야 한다. Baseball.jsx에서 value={v} index={i}로 선언하였으니 value와 index로 가져와야 한다.
-            // <li key={this.props.value.fruit + this.props.value.taste}><b>{this.props.index} - {this.props.value.fruit}</b> - {this.props.value.taste}</li>
-            <li style={{listStyle:"none"}}>
-                {/* tryInfo 안에는 try와 result가 있기 때문에 */}
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
-            </li>
-        );
-    }
-}
+// Hook
+const Try = ({tryInfo}) => { // 구조분해한 {tryInfo}를 props로 한다면 props.tryInfo.try
+    return (
+        <li style={{listStyle:"none"}}>
+            <div>{tryInfo.try}</div>
+            <div>{tryInfo.result}</div>
+        </li>
+    )
+};
+
+// class Try extends Component { // Component => 재사용성
+//     render() {
+//         return (
+//             // 부모 .jsx에서 넘긴 이름 그대로 가져와야 한다. Baseball.jsx에서 value={v} index={i}로 선언하였으니 value와 index로 가져와야 한다.
+//             // <li key={this.props.value.fruit + this.props.value.taste}><b>{this.props.index} - {this.props.value.fruit}</b> - {this.props.value.taste}</li>
+//             <li style={{listStyle:"none"}}>
+//                 {/* tryInfo 안에는 try와 result가 있기 때문에 */}
+//                 <div>{this.props.tryInfo.try}</div>
+//                 <div>{this.props.tryInfo.result}</div>
+//             </li>
+//         );
+//     }
+// }
 
 export default Try;
