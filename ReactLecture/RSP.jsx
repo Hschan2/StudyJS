5c5fe7a176a560b4b27add4de3eaac04b0382511
HONGSEONGCHAN <seongchan_@naver.com>
2020-12-13 15:05:36 +0900

    가위바위보 게임

--- ReactLecture/RSP.jsx
+++ ReactLecture/RSP.jsx
@@ -14,6 +14,12 @@ const scores = {
     보: -1
 };
 
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v) {
+        return v[1] === imgCoord;
+    })[0];
+};
+
 // 실행 순서
 // Class = Constructor -> render -> ref -> componentDidMount -> (setState/props 바뀔 때 -> shouldComponentUpdate -> render -> componentDidUpdate) -> 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
 // shouldComponentUpdate = true일 경우 리렌더링 이 후 componentDidUpdate, false일 경우 리렌더링 X
@@ -32,29 +38,13 @@ class RSP extends Component {
     // setState를 쓰고 싶은데 어디다 써야될지 모를 때
     // 비동기 요청 많이 함
     componentDidMount() {
-        this.interval = setInterval(() => {
-            const {imgCoord} = this.state; // 비동기 함수는 비동기 함수 바깥 것을 참조하면 클로저 문제 발생
-
-            if(imgCoord === rspCoords.바위) {
-                this.setState({
-                    imgCoord: rspCoords.가위
-                });
-            } else if(imgCoord === rspCoords.가위) {
-                this.setState({
-                    imgCoord: rspCoords.보
-                });
-            } else if(imgCoord === rspCoords.보) {
-                this.setState({
-                    imgCoord: rspCoords.바위
-                });
-            }
-        }, 1000); // 1초마다 반복 작업
+        this.interval = setInterval(this.changeHand, 100); // 0.1초마다 반복 작업
     }
 
     // reRender된 후에 실행
-    componentDidUpdate() {
+    // componentDidUpdate() {
 
-    }
+    // }
 
     // 컴포넌트가 제거되기 직전
     // componentDidMount에서 실행된 것을 제거
@@ -63,8 +53,56 @@ class RSP extends Component {
         clearInterval();
     }
 
-    onClickBtn = (choose) => {
+    changeHand = () => {
+        const {imgCoord} = this.state;
 
+        if(imgCoord === rspCoords.바위) {
+            this.setState({
+                imgCoord: rspCoords.가위
+            });
+        } else if(imgCoord === rspCoords.가위) {
+            this.setState({
+                imgCoord: rspCoords.보
+            });
+        } else if(imgCoord === rspCoords.보) {
+            this.setState({
+                imgCoord: rspCoords.바위
+            });
+        }
+    };
+
+    onClickBtn = (choose) => {
+        const { imgCoord } = this.state;
+        clearInterval(this.interval); // Interval 멈춤
+        const myScore = scores[choose];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+
+        // 점수 차이가 없을 시. 즉, 무승부
+        if(diff === 0) {
+            this.setState({
+                result: '비겼다',
+            });
+        } else if([-1, 2].includes(diff)) {
+            this.setState((prevState) => { // 인자 실행할 때는 함수형으로 (return)
+                return {
+                    result: '승리',
+                    score: prevState + 1,
+                };
+            });
+        } else {
+            this.setState((prevState) => { // 인자 실행할 때는 함수형으로 (return)
+                return {
+                    result: '패배',
+                    score: prevState - 1,
+                };
+            });
+        }
+
+        // 게임 후, 2초 뒤 다시 실행
+        setTimeout(() => {
+            this.interval = setInterval(this.changeHand, 1000);
+        }, 2000);
     };
 
     render() {
