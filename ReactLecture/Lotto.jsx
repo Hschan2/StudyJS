3ec415f443539b43ae6f4869d9d6c5cd624bbcd2
HONGSEONGCHAN <seongchan_@naver.com>
2020-12-28 21:17:36 +0900

    Lotto Game (componentDidMount)

--- ReactLecture/Lotto.jsx
+++ ReactLecture/Lotto.jsx
@@ -27,16 +27,62 @@ class Lotto extends Component {
         redo: false,
     };
 
+    // 클리어 작업을 위해, Hook에서 useRef 대신
+    timeouts = [];
+
+    // 실제로 로또 번호 값 넣어주기 (setTimeout 사용)
+    componentDidMount() {
+        const {winNumbers} = this.state;
+        for(let i = 0; i < winNumbers.length - 1; i++) {
+            this.timeouts[i] = setTimeout(() => {
+                this.setState((prevState) => {
+                    return {
+                        winBalls: [...prevState.winBalls, winNumbers[i]],
+                    };
+                });
+            }, (i + 1) * 1000); // 첫 번째 공은 1초, 두 번째 공은 2초... (setTimeout 반복)
+        }
+        this.timeouts[6] = setTimeout(() => { // 보너스 공 가져오기. 보너스 공은 일곱 번째 공이기 때문에 7초 후
+            this.setState({
+                bonus: winNumbers[6],
+                redo: true, // 한 번 더 버튼 보이기
+            });
+        }, 7000);
+    }
+
+    // 공 넣어주기 끝나면 setTimeout 종료
+    // setTimeout 클리어 작업, 메모리 누수되는 것을 막음
+    // setInterval 같은 메모리 상에 누적되는 것을 위해 사용
+    componentWillUnmount() {
+        this.timeouts.forEach((v) => {
+            clearTimeout(v);
+        });
+    }
+
+    // 다시 뽑기
+    onClickRedo = () => {
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        });
+        this.timeouts = [];
+    };
+
     render() {
         const {winBalls, bonus, redo} = this.state;
         return (
             <>
                 <div>로또</div>
                 <div id = "결과창">
+                    {/* 반복문 */}
                     {winBalls.map((v) => <Ball key={v} number={v} />)}
                 </div>
                 <div>보너스 숫자</div>
-                <button onClick = {redo ? this.onClickRedo : () => {}}>번호 추출</button>
+                {/* 조건문 */}
+                {bonus && <Ball number = {bonus} />}
+                {redo && <button onClick = {this.onClickRedo}>번호 추출</button>}
             </>
         );
     }
