63eabd7739d9c273f1f35046981ceb4b597db313
HONGSEONGCHAN <seongchan_@naver.com>
2021-02-25 18:38:59 +0900

    Modify: Name

---
+++ ReactLecture/webRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import GuGuDan from './GuGuDan';
+import Ball from './Ball';
+import Lotto from './Lotto';
+
+const webRouter = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                {/* path => link, component => 실제로 보여질 것 */}
+                {/* Router가 자동으로 컴포넌트를 연결지어 준다 */}
+                <Route path = "/GuGuDan" component = {GuGuDan}></Route>
+                <Route path = "/Ball" component = {Ball}></Route>
+                <Route path = "/Lotto" component = {Lotto}></Route>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default webRouter;
