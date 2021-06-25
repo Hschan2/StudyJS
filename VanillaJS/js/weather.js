dfa5cea83001fe962f6483cc4b49c458a25a9be2
HONGSEONGCHAN <seongchan_@naver.com>
2021-06-25 22:17:04 +0900

    Create: Loading the Weather (OpenWeatherMap API)

--- VanillaJS/js/weather.js
+++ VanillaJS/js/weather.js
@@ -1,6 +1,17 @@
+const APP_KEY = "a03004bf971234fd4cb532f6df20b7af";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const lon = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric`;
+
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${Math.round(data.main.temp)}`;
+    }); // URL fetch -> response -> data 추출
 }
 
 function onGeoError() {
