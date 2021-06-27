568f2477bb08d457f938aaeed78f09dcd1bf5361
HONGSEONGCHAN <seongchan_@naver.com>
2021-06-27 20:12:43 +0900

    Modify: Weather 출력 텍스트 수정

--- VanillaJS/js/weather.js
+++ VanillaJS/js/weather.js
@@ -4,7 +4,7 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric&p=korean`;
 
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
@@ -18,7 +18,7 @@ function onGeoOk(position) {
         else if(data.weather[0].main === "Thunderstorm") data.weather[0].main = "번개";
 
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} ${Math.round(data.main.temp)}˚ `;
+        weather.innerText = `${data.weather[0].main} ${Math.round(data.main.temp)}˚ / `;
     }); // URL fetch -> response -> data 추출
 }
 
