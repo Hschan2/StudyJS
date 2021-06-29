bf3c2d2c5032b7fc8385159387114fc533d0ad05
HONGSEONGCHAN <seongchan_@naver.com>
2021-06-29 17:26:23 +0900

    Modify: OpenWeatherMap API 수정

--- VanillaJS/js/weather.js
+++ VanillaJS/js/weather.js
@@ -3,8 +3,9 @@ const APP_KEY = "a03004bf971234fd4cb532f6df20b7af";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+    const lang = "kr";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric&lang=kr`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric&lang=${lang}`;
 
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
