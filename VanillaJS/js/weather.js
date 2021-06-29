ced89a303e4b3a4ddcb2192931ac8e90df183812
HONGSEONGCHAN <seongchan_@naver.com>
2021-06-29 13:12:15 +0900

    Modify: OpenWeatherMap에서 lang=kr 추가

--- VanillaJS/js/weather.js
+++ VanillaJS/js/weather.js
@@ -4,18 +4,18 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric&p=korean`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_KEY}&units=metric&lang=kr`;
 
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
 
-        if (data.weather[0].main === "Clear") data.weather[0].main = "맑음";
-        else if(data.weather[0].main === "Snow") data.weather[0].main = "눈";
-        else if(data.weather[0].main === "Clouds") data.weather[0].main = "흐림";
-        else if(data.weather[0].main === "Rain") data.weather[0].main = "비";
-        else if(data.weather[0].main === "Drizzle") data.weather[0].main = "이슬비";
-        else if(data.weather[0].main === "Thunderstorm") data.weather[0].main = "번개";
+        // if (data.weather[0].main === "Clear") data.weather[0].main = "맑음";
+        // else if(data.weather[0].main === "Snow") data.weather[0].main = "눈";
+        // else if(data.weather[0].main === "Clouds") data.weather[0].main = "흐림";
+        // else if(data.weather[0].main === "Rain") data.weather[0].main = "비";
+        // else if(data.weather[0].main === "Drizzle") data.weather[0].main = "이슬비";
+        // else if(data.weather[0].main === "Thunderstorm") data.weather[0].main = "번개";
 
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} ${Math.round(data.main.temp)}˚ / `;
