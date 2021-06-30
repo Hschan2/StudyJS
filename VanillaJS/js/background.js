38bd9e77b9931ff5811766c8aab1951a57fcaa23
HONGSEONGCHAN <seongchan_@naver.com>
2021-06-30 20:27:51 +0900

    Modify: 배경 동영상으로 변경

--- VanillaJS/js/background.js
+++ VanillaJS/js/background.js
@@ -1,4 +1,5 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"];
+// const images = ["1.jpg", "2.jpg", "3.jpg"];
+const images = ["1.MOV", "2.MOV", "3.MOV"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
