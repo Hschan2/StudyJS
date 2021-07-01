c00618a2ac7df144c02a7e24f3f91c8b9d23884c
HONGSEONGCHAN <seongchan_@naver.com>
2021-07-01 19:04:56 +0900

    Modify: Add Videos

--- VanillaJS/js/background.js
+++ VanillaJS/js/background.js
@@ -1,5 +1,5 @@
 // const images = ["1.jpg", "2.jpg", "3.jpg"];
-const images = ["1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4", "6.mp4", "7.mp4"];
+const images = ["1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4", "6.mp4", "7.mp4", "8.mp4", "9.mp4", "10.mp4", "11.mp4"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
