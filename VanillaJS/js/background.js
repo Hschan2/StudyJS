5ae62844a3b0875f1ff9fb0581cd7c948f4d4f18
HONGSEONGCHAN <seongchan_@naver.com>
2021-07-01 15:50:58 +0900

    Modify: 배경 이미지에서 동영상으로 변경

--- VanillaJS/js/background.js
+++ VanillaJS/js/background.js
@@ -1,5 +1,5 @@
 // const images = ["1.jpg", "2.jpg", "3.jpg"];
-const images = ["1.MOV", "2.MOV", "3.MOV"];
+const images = ["1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4", "6.mp4", "7.mp4"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
